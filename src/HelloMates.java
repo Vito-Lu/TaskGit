e0f0ac6869a75c4a790bdd39ded4f6b5b5da1d9f
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-03-06 17:09:02 +0200

    All removed

--- src/HelloMates.java
+++
@@ -1,5 +0,0 @@
-public class HelloMates {
-    public static void main(String[] args) {
-        System.out.println("Hello Mates!");
-    }
-}
--- src/MatchResult.java
+++
@@ -1,6 +0,0 @@
-public class MatchResult {
-    public int goal(int firstTimeFirstCrew, int secondTimeFirstCrew,  int firstTimeSecondCrew, int secondTimeSecondCrew = 1; ) {
-        int goalAll = firstTimeFirstCrew+ secondTimeFirstCrew+ firstTimeSecondCrew+ firstTimeSecondCrew;
-        return goalAll;
-    }
-}
